7304300c0ce0aae512804d6de09daa365a724b91
Jocs <jhassan8@outlook.com>
2019-11-10 19:45:00 -0300

    Create import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,16 @@
+INSERT INTO clients(name,surname,email,create_at) VALUES ('Noel','Koch','noelkoch@elentrix.com','2016-10-09');
+INSERT INTO clients(name,surname,email,create_at) VALUES ('Dorthy','Peters','dorthypeters@elentrix.com','2019-08-19');
+INSERT INTO clients(name,surname,email,create_at) VALUES ('Tameka','Mendez','tamekamendez@elentrix.com','2016-03-23');
+INSERT INTO clients(name,surname,email,create_at) VALUES ('Catalina','Wheeler','catalinawheeler@elentrix.com','2014-08-11');
+INSERT INTO clients(name,surname,email,create_at) VALUES ('Rosetta','Stout','rosettastout@elentrix.com','2016-03-01');
+INSERT INTO clients(name,surname,email,create_at) VALUES ('Bridgett','Peck','bridgettpeck@elentrix.com','2018-06-26');
+INSERT INTO clients(name,surname,email,create_at) VALUES ('Latoya','William','latoyawilliam@elentrix.com','2018-04-14');
+INSERT INTO clients(name,surname,email,create_at) VALUES ('Leach','Parks','leachparks@elentrix.com','2015-06-05');
+INSERT INTO clients(name,surname,email,create_at) VALUES ('Howe','Manning','howemanning@elentrix.com','2017-01-31');
+INSERT INTO clients(name,surname,email,create_at) VALUES ('Sullivan','Evans','sullivanevans@elentrix.com','2018-03-17');
+INSERT INTO clients(name,surname,email,create_at) VALUES ('Ross','Perkins','rossperkins@elentrix.com','2017-01-14');
+INSERT INTO clients(name,surname,email,create_at) VALUES ('Whitley','Castaneda','whitleycastaneda@elentrix.com','2019-07-29');
+INSERT INTO clients(name,surname,email,create_at) VALUES ('Cantu','Pugh','cantupugh@elentrix.com','2015-12-16');
+INSERT INTO clients(name,surname,email,create_at) VALUES ('Mckinney','Le','mckinneyle@elentrix.com','2014-05-23');
+INSERT INTO clients(name,surname,email,create_at) VALUES ('Irma','Mcintosh','irmamcintosh@elentrix.com','2019-08-11');
+INSERT INTO clients(name,surname,email,create_at) VALUES ('Alyce','Harrington','alyceharrington@elentrix.com','2017-05-01');
